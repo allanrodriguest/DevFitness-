0b6257c53487c3d964c9a94c4f075975fbce63e7
Allan Rodrigues <60073178+allanrodriguest@users.noreply.github.com>
2021-06-07 02:32:21 -0300

    Controllers added

---
+++ Controllers/MealsController.cs
@@ -0,0 +1,42 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFitness.API.Controllers
+{
+   [Route("api/users/{userId}/meals")]
+   public class MealsController : ControllerBase
+   {
+      [HttpGet]
+      public IActionResult GetAll(int userId)
+      {
+         return Ok();
+      }
+
+      [HttpGet("{mealId}")]
+      public IActionResult Get(int userId, int mealId)
+      {
+         return Ok();
+      }
+
+      [HttpPost]
+
+      public IActionResult Post (int userId, [FromBody] CreateMealInputModel inputModel)
+      {
+         return Ok();
+      }
+
+      [HttpPut("{mealId}")]
+
+      public IActionResult Put(int userId, int mealId, [FromBody] UpdateMealInputModel inputModel)
+      {
+         return NoContent();
+      }
+
+      [HttpDelete("{mealId}")]
+
+      public IActionResult Delete(int userId, int mealId)
+      {
+         return NoContent();
+      }
+   }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFitness.API.Controllers
+{
+   [Route("api/[controller]")]
+   public class UsersController : ControllerBase
+   {
+      [HttpGet("{id}")]
+
+      public IActionResult Get(int id)
+      {
+         return Ok();
+      }
+
+      [HttpPost]
+
+      public IActionResult Post([FromBody] CreateUserInputModel inputModel)
+      {
+         return CreatedAtAction(nameof(Get), new { id = 10 }, inputModel);
+      }
+
+      [HttpPut("{id}")]
+      public IActionResult Put(int id, [FromBody] UpdateUserInputModel inputModel)
+      {
+         return NoContent();
+      }
+   }
+}
